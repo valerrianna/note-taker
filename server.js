74cd6af465c93c81900c5d82775fecb94fbcf2dd
Valerie <valerie.tram.anh.n@gmail.com>
2021-11-07 12:26:13 -0500

    got POST to work

--- server.js
+++ server.js
@@ -1,12 +1,39 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const { notes } = require('./db/db.json');
 
+// parse incoming string or array data
+app.use(express.urlencoded({ extended: true }));
+// parse incoming JSON data
+app.use(express.json());
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({notes: notesArray}, null, 2)
+    );
+    return note;
+}
+
 app.get('/api/notes', (req, res) => {
     res.json(notes)
 });
+
+app.post('/api/notes', (req, res) => {
+    // set id based on what the next index of the array will be
+    req.body.id = notes.length.toString();
+  
+    // add animal to json file and animals array in this function
+    const note = createNewNote(req.body, notes);
+    res.json(note);
+});
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
