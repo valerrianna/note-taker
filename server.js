b0f9afb890ce9a553a9e2c218533224a157fb938
Valerie <valerie.tram.anh.n@gmail.com>
2021-11-07 14:15:52 -0500

    create other routes to server other pages

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
 
+app.use(express.static('public'));
+
 function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
@@ -34,6 +36,17 @@ app.post('/api/notes', (req, res) => {
     res.json(note);
 });
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
